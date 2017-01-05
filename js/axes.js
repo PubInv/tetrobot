533e40c3380d5670289084cdc58b631b4c42f436
Robert L. Read <read.robert@gmail.com>
2017-01-05 17:43:35 -0600

    zeroing axes

--- js/axes.js
+++ js/axes.js
@@ -29,18 +29,71 @@ function createAGrid(opts) {
     stepw = 2 * config.width / config.linesWidth,
     steph = 2 * config.height / config.linesHeight;
 
-  //width
-  for (var i = -config.width; i <= config.width; i += stepw) {
-    gridGeo.vertices.push(new THREE.Vector3(-config.height, i, 0));
-    gridGeo.vertices.push(new THREE.Vector3(config.height, i, 0));
+    // This code creates a grid, but not centered on zero....
+  // //width
+  // for (var i = -config.width; i <= config.width; i += stepw) {
+  //   gridGeo.vertices.push(new THREE.Vector3(-config.height, i, 0));
+  //   gridGeo.vertices.push(new THREE.Vector3(config.height, i, 0));
 
-  }
-  //height
-  for (var i = -config.height; i <= config.height; i += steph) {
-    gridGeo.vertices.push(new THREE.Vector3(i, -config.width, 0));
-    gridGeo.vertices.push(new THREE.Vector3(i, config.width, 0));
-  }
+  // }
+  // //height
+  // for (var i = -config.height; i <= config.height; i += steph) {
+  //   gridGeo.vertices.push(new THREE.Vector3(i, -config.width, 0));
+  //   gridGeo.vertices.push(new THREE.Vector3(i, config.width, 0));
+  // }
 
+    //width
+    for (var i = -config.width; i <= config.width; i += stepw) {
+      gridGeo.vertices.push(new THREE.Vector3(-config.height, i, 0));
+      gridGeo.vertices.push(new THREE.Vector3(config.height, i, 0));
+    }
+    
+    //height
+    for (var i = -config.height; i <= config.height; i += steph) {
+      gridGeo.vertices.push(new THREE.Vector3(i, -config.width, 0));
+      gridGeo.vertices.push(new THREE.Vector3(i, config.width, 0));
+    }
+    
+  var line = new THREE.Line(gridGeo, material, THREE.LinePieces);
+  gridObject.add(line);
+
+  return gridObject;
+}
+
+function createZeroedGrid(opts) {
+  var config = opts || {
+    cellheight: 1.0,
+    cellwidth: 1.0,
+    numCellsPosHeight: 10,
+    numCellsPosWidth: 10,
+    color: 0xDD006C
+  };
+
+  var material = new THREE.LineBasicMaterial({
+    color: config.color,
+      opacity: 0.2,
+      linewidth: 2
+  });
+
+  var gridObject = new THREE.Object3D(),
+    gridGeo = new THREE.Geometry(),
+    stepw = config.cellwidth,
+    steph = config.cellheight;
+
+    //width
+    var halfheight = config.cellheight*config.numCellsPosHeight;
+    var halfwidth = config.cellwidth*config.numCellsPosWidth;
+    for (var i = -config.numCellsPosWidth; i <= config.numCellsPosWidth; i += stepw) {
+      gridGeo.vertices.push(new THREE.Vector3(-halfheight, i*config.cellwidth, 0));
+	gridGeo.vertices.push(new THREE.Vector3(halfheight, i*config.cellwidth, 0));
+    }
+    
+    //height
+    for (var i = -config.numCellsPosHeight; i <= config.numCellsPosHeight; i += steph) {
+      gridGeo.vertices.push(new THREE.Vector3(i*config.cellheight, -halfwidth, 0));
+	gridGeo.vertices.push(new THREE.Vector3(i*config.cellheight, halfwidth, 0));
+    }
+    
   var line = new THREE.Line(gridGeo, material, THREE.LinePieces);
   gridObject.add(line);
 
@@ -48,19 +101,20 @@ function createAGrid(opts) {
 }
 
 function labelAxis(width, data, direction){
+    var w = width;
 
   var separator = 2*width/data.length,
 			p = {
-				x:0,
-				y:0,
-				z:0
+				x:-w,
+				y:-w,
+				z:-w
 			},
 			dobj = new THREE.Object3D();
 
   for ( var i = 0; i < data.length; i ++ ) {
       var label = makeTextSprite(data[i],{fontsize: 50 });
 
-		label.position.set(p.x,p.y,p.z);
+		label.position.set(p.x+w,p.y+w,p.z+w);
 
 		dobj.add( label );
 		if (direction=="y"){
@@ -106,28 +160,39 @@ function makeTextSprite( message, parameters )
 	return sprite;
 }
 
-function getMeterScale(sep,meters) {
+function getMeterScale(sep,meters,offset) {
     var scale = [];
-    var n = meters/sep;
-    for(var i = 0; i < n; i++) {
-	scale.push(""+i*sep+"m");
+    var n = Math.ceil(meters/sep);
+    for(var i = 0; i <= n; i++) {
+	var v = i*sep + offset;
+	scale.push(""+v+"m");
     }
     return scale;
 }
 
+function getBalancedMeterScale(sep,meters,offset) {
+    return getMeterScale(sep,meters,-(meters/2.0));
+}
+function getPositiveMeterScale(sep,meters) {
+    return getMeterScale(sep,meters,0);
+}
+    
+
 function gridInit(glScene,gDimensions){
 
-    var tickSeparationMeters = 0.5;
+    var tickSeparationMeters = 1.0;
 
     // Y is the vertical dimension!
     // x is width, z is distance away
     var data = {
   labels: {
-      y: getMeterScale(tickSeparationMeters,gDimensions.h),
-      x: getMeterScale(tickSeparationMeters,gDimensions.w),
-    z: getMeterScale(tickSeparationMeters,gDimensions.d)
+      y: getPositiveMeterScale(tickSeparationMeters,gDimensions.h),
+      x: getBalancedMeterScale(tickSeparationMeters,gDimensions.w),
+    z: getBalancedMeterScale(tickSeparationMeters,gDimensions.d)
   }
-};
+    };
+
+    var cell = 1.0;
 
 
       var graphDimensions = {
@@ -145,11 +210,11 @@ function gridInit(glScene,gDimensions){
 			c= data.labels.z.length;
 
 	//pink
-	var newGridXY = createAGrid({
-				height: width,
-				width: height,
-				linesHeight: b,
-				linesWidth: a,
+	var newGridXY = createZeroedGrid({
+	    cellheight: cell,
+	    cellwidth: cell,
+	    numCellsPosHeight: (width/cell),
+	    numCellsPosWidth: (height/cell),
 				color: 0xcc0000
 			});
 			//newGridXY.position.y = height;
@@ -158,51 +223,52 @@ function gridInit(glScene,gDimensions){
 			boundingGrid.add(newGridXY);
 
     //blue
-	var newGridYZ = createAGrid({
-				height: width,
-				width: depth,
-				linesHeight: b,
-				linesWidth: c,
-				color: 0x0000cc
-			});
+	var newGridYZ = createZeroedGrid({
+	    cellheight: cell,
+	    cellwidth: cell,
+	    numCellsPosHeight: (graphDimensions.d/cell)/2,
+	    numCellsPosWidth: (graphDimensions.w/cell)/2,
+	    color: 0x0000cc
+	});
 	 		newGridYZ.rotation.x = Math.PI/2;
 //	 		newGridYZ.position.y = -height;
 			boundingGrid.add(newGridYZ);
 
 	//green
-	var newGridXZ = createAGrid({
-				height: depth,
-				width: height,
-				linesHeight:c,
-				linesWidth: a,
+	var newGridXZ = createZeroedGrid({
+	    cellheight: cell,
+	    cellwidth: cell,
+	    numCellsPosHeight: (width/cell),
+	    numCellsPosWidth: (height/cell),
 				color: 0x00cc00
 			});
 
-    newGridXZ.position.x = width;
+        newGridXZ.position.x = width;
         newGridXZ.position.y = height;
 			//newGridXZ.position.y = height;
-	 		newGridXZ.rotation.y = Math.PI/2;
+    newGridXZ.rotation.y = Math.PI/2;
+    
 			boundingGrid.add(newGridXZ);
 
     glScene.add(boundingGrid);
 
-    var margin = 0.4;
+    var margin = -0.5;
 	var labelsW = labelAxis(width, data.labels.x,"x");
 			labelsW.position.x = width;
 			labelsW.position.y = 0;
-    			labelsW.position.z = -margin + -depth;
+    			labelsW.position.z = -depth;
 			glScene.add(labelsW);
 
 	var labelsH = labelAxis(height, data.labels.y,"y");
 			labelsH.position.x = width;
-                        labelsH.position.y = -margin/2;
+                        labelsH.position.y = -0.5/2;
 			labelsH.position.z = -depth;
 			glScene.add(labelsH);
 
 	var labelsD = labelAxis(depth, data.labels.z, "z");
                         labelsD.position.x = width;
                         labelsD.position.y = 0;
-			labelsD.position.z = -margin + depth;
+			labelsD.position.z = 0.0 + depth;
 			glScene.add(labelsD);
 };
 
