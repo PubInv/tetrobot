d5cd425fd048f4ef7f04bea48a89b4f2b1919666
Robert L. Read <read.robert@gmail.com>
2017-04-04 10:14:08 -0500

    significant work

--- generateTetrahelix.js
+++ generateTetrahelix.js
@@ -5,84 +5,57 @@
 // simplification and derivation on this tree.
 
 // load math.js (using node.js)
-var math = require('mathjs');
+const math = require('mathjs');
+const assert = require('assert');
+
 
 var red_phase = 0;
 var yellow_phase = 1;
 var blue_phase = 2;
 
-var BCrot = math.acos(math.divide(math.bignumber(-2),math.bignumber(3)));
-var BCH = (1/math.sqrt(10));
-function Cox(n) {
-    console.log("cox",n);
-    var pnt = [];
-    var r = (3 * math.sqrt(3) / 10);
-    var h = BCH;
-    var theta = Math.acos(-2/3);
-    pnt[0] = r * math.cos(n*theta);
-    pnt[1] = r * math.sin(n*theta);
-    pnt[2] = n*h;
-    return pnt;
-}
+var BCtheta = math.acos(math.divide(math.bignumber(-2),math.bignumber(3)));
+var BCrho = 3*BCtheta - 2 * Math.PI;
+var BCh = (1/math.sqrt(10));
+var BCd = (3/math.sqrt(10));
+var BCr = (3 * math.sqrt(3) / 10);
 
-function CoxHelix(k,phase) {
-    return Cox(k*3 +  math.number(phase));
-}
+// This from https://github.com/josdejong/mathjs/blob/develop/lib/utils/bignumber/nearlyEqual.js
+function nearlyEqual(x, y, epsilon) {
+  // if epsilon is null or undefined, test whether x and y are exactly equal
+  if (epsilon == null) {
+    return x === y;
+  }
 
-function RedHelix(n) {
-    var pnt = [];
-    var r = (3 * math.sqrt(3) / 10);
-    var h = (3*BCH);
-    var theta = Math.acos(-2/3);
-    pnt[0] = r * math.cos(n * 3 * theta);
-    pnt[1] = r * math.sin(n * 3 * theta);
-    pnt[2] = n*h;
-    return pnt;
-    
-}
 
-// This seems to be working!! So now we have in fact defined a helix
-// Note: I think this is correct, but the parameter is interpret as "node number".
-// We would like a version of this parameterized by z position!
-function ColorHelix(n,phase) {
-    var pnt = [];
-    var r = (3 * math.sqrt(3) / 10);
-    var h = BCH;
-    var theta = math.acos(-2/3);
-    var slow = (3*theta - math.pi*2);
-    var third = 2*math.pi/3;
-    var angle = slow*n+ phase*theta;
-    pnt[0] = r * math.cos(angle);
-    pnt[1] = r * math.sin(angle);
-    console.log("ColorHelix",phase/3);
-    pnt[2] = (n+phase/3) * 3 * h;
-    return pnt;
-}
+  // use "==" operator, handles infinities
+  if (x == y) {
+    return true;
+  }
 
-function NColorHelix(n,phase) {
-    var pnt = [];
-    var r = (3 * math.sqrt(3) / 10);
-    var h = BCH;
-    var x = n+phase/3.0;
-    var theta = math.acos(-2/3);
-    var slow = (3*theta - math.pi*2);
-    var third = 2*math.pi/3.0;
-    // Note the subtle distinct here...
-    // in fact:
-    //   slow *(n+phase/3.0) + phase*third == slow * n + phase*theta!
-    //
-    var angle = slow*x+ phase*third;
-    pnt[0] = r * math.cos(angle);
-    pnt[1] = r * math.sin(angle);
-    pnt[2] = (x) * 3 * h;
-    return pnt;
-}
+  // NaN
+  if (isNaN(x) || isNaN(y)) {
+    return false;
+  }
+
+    // check numbers are very close, needed when comparing numbers near zero
+    var diff = math.abs(x-y);
+    if (diff == 0) {
+      return true;
+    }
+    else {
+	if (diff < epsilon)
+	    return true;
+    }
+
+  // Infinite and Number or negative Infinite and positive Infinite cases
+  return false;
+};
 
 function PColorHelix(n,phase,lambda) {
     var pnt = [];
     var r = (3 * math.sqrt(3) / 10);
     var rl = (r - 1/math.sqrt(3))*lambda + 1/math.sqrt(3);
-    var h = BCH;
+    var h = BCh;
     var hl = (h - 1/3)*lambda + 1/3;    
     var x = n+phase/3.0;
     var theta = math.acos(-2/3);
@@ -99,49 +72,81 @@ function PColorHelix(n,phase,lambda) {
     return pnt;
 }
 
-// This is now working --- I need to comute an initial phase.
-// NOTE: This seems to work for Lambda = 0.0 and lambda = 1.0, but
-// not the intermediate values. I need to think carefully about it.
+function find_drho_from_r(rho,r) {
+    var sin_r_2 = math.sin(rho/2);
+    var d = math.sqrt(1 - 4 * r * r * sin_r_2 * sin_r_2);
+    return d;
+}
 
-function Helix(x,lambda,phase) {
+function find_rrho_from_d(rho,d) {
+    var sin_r_2 = math.sin(rho/2);
+    console.log("sin_r_2",sin_r_2);
+    console.log("math.sqrt(1 - d*d)",math.sqrt(1 - d*d));
+    console.log("2 * sin_r_2", 2 * sin_r_2);
+    var r = math.sqrt(1 - d*d) / (2 * sin_r_2);
+    return r;
+}
 
-//    var twist = 45*(math.pi / 180.0);
+
+function H_general(n,c,rho,d,r) {
     var pnt = [];
-
-    // radius must be 1/sqrt(3) when Lambda = 0, 3*sqrt(3)/10 when lambda = 1.0
-    var sqrt3inverse = math.divide(math.bignumber(1),math.sqrt(math.bignumber(3)));
-    
-    var sqrt3over10time3 = math.divide(math.multiply(math.bignumber(3),math.sqrt(math.bignumber(3))),math.bignumber(10));
-    console.log("sqrt3 inverse: " + sqrt3inverse)
-    console.log("sqrt3inverse inverse: " + sqrt3over10time3)    
-    
-    var radius = math.add(math.multiply(math.subtract(sqrt3over10time3,sqrt3inverse),lambda),sqrt3inverse);
-    console.log("radius: " + radius)    
-    // the height factor determines the x displacements of nodes of the same color.
-    // It is 1.0 when lambda is zero and 3/sqrt(10.0) when lambda = 1.
-    var threeoversqrt10 = math.divide(math.bignumber(3),math.sqrt(math.bignumber(10))) 
-    var h = math.add(math.multiply(
-	math.subtract(threeoversqrt10,math.bignumber(1)),
-	lambda),math.bignumber(1));
-    pnt[0] = math.multiply(x,h);
-    console.log("h: "+ h);
-    var a = math.multiply(x,lambda);
-//    var b = math.divide(BCrot,h);
-    console.log("a: "+ a);
-//    console.log("b: "+ b);
-    //    var rotation = math.add(math.multiply(a,BCrot),phase);
-    var rotation = math.multiply(a,BCrot);    
-    console.log("rotation: "+ rotation);
-    pnt[1] = math.multiply(radius,math.bignumber(math.cos(rotation)));
-
-    pnt[2] = math.multiply(radius,math.bignumber(math.sin(rotation)));
+    var kappa = n+ c/3.0;
+    var rk = rho*kappa;
+    var angle = rk + c*2*math.PI/3;
+    pnt[0] = r*math.cos(angle);
+    pnt[1] = r*math.sin(angle);
+    pnt[2] = d*kappa;
     return pnt;
+}
+
+function H_bc(n,c) {
+    var BCr = find_rrho_from_d(BCrho,BCd);
+    return H_general(n,c,BCrho,BCd,BCr);
+}
+
+
+// WARNING: This is an error...it is not at all clear what this should be.
+// The BC helix has a particular value, but unless we can compute
+// a minimization error and hope that we can use interpolation thereof,
+// then this will be difficult.
+function H_bc_lambda(n,c,lambda) {
+    // Note this is a particular scheme for parametrization.
+    // Now we must compute r and h....
+//    var h = BCh;    
+    var dl = (BCd - 1)*lambda + 1;
+    var rhol = BCrho * lambda;
+    // having fixed dl and rhol, we must find r    
+    var rl = (lambda == 0) ?  1/math.sqrt(3) : find_rrho_from_d(rhol,dl);
+    console.log("rl");
+    console.log(rl);
+    return H_general(n,c,rhol,dl,rl);
+}
+
+function test_rail_angle_formula_against_BC() {
+    var BCr_check = find_rrho_from_d(BCrho,BCd);
+    var BCd_check = find_drho_from_r(BCrho,BCr);
+    assert(nearlyEqual(BCr,BCr_check,0.0000001),
+	   'BCr nearly equal fail '+BCr+', BCr_check == '+BCr_check);    
+    assert(nearlyEqual(BCd,BCd_check,0.0000001),
+	   'BCd == '+BCd+', BCd_check == '+BCd_check);
+}
+
+function test_H_general_against_BC() {
+    var R0 = H_bc(0,0);
+    var R1 = H_bc(1,0);
+    var Y0 = H_bc(0,1);
+    var B0 = H_bc(0,2);
+    assert(nearlyEqual(math.distance(R0,R1),1,0.0000001),"\n R0 = " + R0 +"\n R1 = " + R1 +"\n distance =" + math.distance(R0,R1));
+    assert(nearlyEqual(math.distance(R0,Y0),1,0.0000001),"\n R0 = " + R0 +"\n Y0 = " + Y0 +"\n distance =" + math.distance(R0,Y0));
+    assert(nearlyEqual(math.distance(R0,B0),1,0.0000001),"\n R0 = " + R0 +"\n B0 = " + B0 +"\n distance =" + math.distance(R0,B0));
+
 }
 
 // simplify an expression
 console.log('Let us compute 100 points in a tetrahelix...');
-console.log("BCrot");
-console.log(BCrot);
+console.log("BCtheta");
+console.log(BCtheta);
+
 var reds = [];
 var otherreds = [];
 var blues = [];
@@ -149,20 +154,23 @@ var otheryells = [];
 var yells = [];
 var otherblues = [];
 // var lambda = 1.0;
-var lambda = 0.0;
+var lambda = 1.0;
 var third = math.multiply(math.bignumber(120),(math.divide(math.bignumber(math.pi),math.bignumber(180.0))));    
 for(var i = 0; i < 4; i++) {
-//    var red = Helix(math.bignumber(i),lambda,math.multiply(third,red_phase));
-//    var yell = Helix(math.bignumber((i+(1/3))),lambda,math.multiply(third,yellow_phase));
-//    var blue = Helix(math.bignumber((i+(2/3))),lambda,math.multiply(third,blue_phase));
-    var red = PColorHelix(i,red_phase,lambda);
-    var otherr = ColorHelix(i,red_phase);
+
+    //    var red = H_bc_lambda(i,red_phase,lambda);
+    var red = H_bc(i,red_phase);    
+    var otherr = PColorHelix(i,red_phase,lambda);
     otherreds.push(otherr);
-    var yell = PColorHelix(i,yellow_phase,lambda);
-    var othery = ColorHelix(i,yellow_phase);
+    
+    //    var yell = H_bc_lambda(i,yellow_phase,lambda);
+    var yell = H_bc(i,yellow_phase);            
+    var othery = PColorHelix(i,yellow_phase,lambda);
     otheryells.push(othery);
-    var blue = PColorHelix(i,blue_phase,lambda);
-    var otherb = ColorHelix(i,blue_phase);
+    
+    //    var blue = H_bc_lambda(i,blue_phase,lambda);
+    var blue = H_bc(i,blue_phase);        
+    var otherb = PColorHelix(i,blue_phase,lambda);
     otherblues.push(otherb);
     
     reds.push(red);
@@ -253,3 +261,5 @@ for(var i = 0; i < 3; i++) {
     
 }
 
+test_rail_angle_formula_against_BC();
+test_H_general_against_BC();
